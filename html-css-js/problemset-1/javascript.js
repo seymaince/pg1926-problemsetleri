9609099a181950282e1d892e9b529abeebbd238a
seymaince <78604928+seymaince@users.noreply.github.com>
2021-02-05 17:40:10 +0300

    Add files via upload

---
+++ html-css-js/problemset-1/javascript.js
@@ -0,0 +1,269 @@
+//1.SATIR
+function a1()
+{
+	alert("A1");
+};
+function b1()
+{
+	alert("B1");
+};
+function c1()
+{
+	alert("C1");
+};
+function d1()
+{
+	alert("D1");
+};
+function e1()
+{
+	alert("E1");
+};
+function f1()
+{
+	alert("F1");
+}
+function g1()
+{
+	alert("G1");
+};
+function h1()
+{
+	alert("H1");
+};
+//2.SATIR
+function a2()
+{
+	alert("A2");
+};
+function b2()
+{
+	alert("B2");
+};
+function c2()
+{
+	alert("C2");
+};
+function d2()
+{
+	alert("D2");
+};
+function e2()
+{
+	alert("E2");
+};
+function f2()
+{
+	alert("F2");
+}
+function g2()
+{
+	alert("G2");
+};
+function h2()
+{
+	alert("H2");
+};
+//3.SATIR
+function a3()
+{
+	alert("A3");
+};
+function b3()
+{
+	alert("B3");
+};
+function c3()
+{
+	alert("C3");
+};
+function d3()
+{
+	alert("D3");
+};
+function e3()
+{
+	alert("E3");
+};
+function f3()
+{
+	alert("F3");
+}
+function g3()
+{
+	alert("G3");
+};
+function h3()
+{
+	alert("H3");
+};
+
+//4.SATIR
+function a4()
+{
+	alert("A4");
+};
+function b4()
+{
+	alert("B4");
+};
+function c4()
+{
+	alert("C4");
+};
+function d4()
+{
+	alert("D4");
+};
+function e4()
+{
+	alert("E4");
+};
+function f4()
+{
+	alert("F4");
+}
+function g4()
+{
+	alert("G4");
+};
+function h4()
+{
+	alert("H4");
+};
+
+//5.SATIR
+function a5()
+{
+	alert("A5");
+};
+function b5()
+{
+	alert("B5");
+};
+function c5()
+{
+	alert("C5");
+};
+function d5()
+{
+	alert("D5");
+};
+function e5()
+{
+	alert("E5");
+};
+function f5()
+{
+	alert("F5");
+}
+function g5()
+{
+	alert("G5");
+};
+function h5()
+{
+	alert("H5");
+};
+
+//6.SATIR
+function a6()
+{
+	alert("A6");
+};
+function b6()
+{
+	alert("B6");
+};
+function c6()
+{
+	alert("C6");
+};
+function d6()
+{
+	alert("D6");
+};
+function e6()
+{
+	alert("E6");
+};
+function f6()
+{
+	alert("F6");
+}
+function g6()
+{
+	alert("G6");
+};
+function h6()
+{
+	alert("H6");
+};
+//7.SATIR
+function a7()
+{
+	alert("A7");
+};
+function b7()
+{
+	alert("B7");
+};
+function c7()
+{
+	alert("C7");
+};
+function d7()
+{
+	alert("D7");
+};
+function e7()
+{
+	alert("E7");
+};
+function f7()
+{
+	alert("F7");
+}
+function g7()
+{
+	alert("G7");
+};
+function h7()
+{
+	alert("H7");
+};
+
+//8.SATIR
+function a8()
+{
+	alert("A8");
+};
+function b8()
+{
+	alert("B8");
+};
+function c8()
+{
+	alert("C8");
+};
+function d8()
+{
+	alert("D8");
+};
+function e8()
+{
+	alert("E8");
+};
+function f8()
+{
+	alert("F8");
+}
+function g8()
+{
+	alert("G8");
+};
+function h8()
+{
+	alert("H8");
+};
+
